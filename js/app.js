e11af0eff44cd6ee2fd5400e0a671fc67723870a
motasemZaidAlkilani <m.kilany1999@gmail.com>
2022-02-10 14:45:19 +0200

    net_salary senior_and_employee capitalized

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 'use strict'
 
 
-let name1 =new employee(1000,"Ghazi Samer","Administration","senior",1000);
-let name2 =new employee(1001,"Lana Ali","finance","senior",1000);
-let name3 =new employee(1002,"Tamara Ayoub","Marketing","senior",1000);
-let name4=new employee(1003,"Safi Walid","Administration","Mid-Senior",1000);
-let name5 =new employee(1004,"Omar Zaid","Development","senior",1000);
-let name6=new employee(1005,"Rana Saleh","Development","Junior",1000);
-let name7 =new employee(1006,"mHadi Ahmad","finance","Mid-Senior",1000);
+let name1 =new Employee(1000,"Ghazi Samer","Administration","Senior",1000);
+let name2 =new Employee(1001,"Lana Ali","finance","Senior",1000);
+let name3 =new Employee(1002,"Tamara Ayoub","Marketing","Senior",1000);
+let name4=new Employee(1003,"Safi Walid","Administration","Mid-Senior",1000);
+let name5 =new Employee(1004,"Omar Zaid","Development","Senior",1000);
+let name6=new Employee(1005,"Rana Saleh","Development","Junior",1000);
+let name7 =new Employee(1006,"mHadi Ahmad","finance","Mid-Senior",1000);
 
 
-  function employee(employeeId,FullName,Department, Level,salary) {
+  function Employee(employeeId,FullName,Department, Level,salary) {
     this. employeeId_var = employeeId;
     this.FullName_var = FullName;
     this.Department_var =  Department;
@@ -20,26 +20,26 @@ let name7 =new employee(1006,"mHadi Ahmad","finance","Mid-Senior",1000);
   }
 
   
-employee.prototype.generateSalary=function(){
-    if(this.Level_var=="senior"){
+  Employee.prototype.generateSalary=function(){
+    if(this.Level_var=="Senior"){
         let random=Math.floor(Math.random() * (2000-1500))+ 1500 ;
-        this.salary_var=random*.075;
+        this.salary_var=random-(random*.075);
         
     }
     else if(this.Level_var =="Mid-Senior"){
         let random=Math.floor(Math.random() * (1500-1000))+1000;
-        this.salary_var=random*.075;
+        this.salary_var=random-(random*.075);
         
     }
     else{
         let random=Math.floor(Math.random() * (1000-500)) + 500;
-        this.salary_var=random*.075;
+        this.salary_var=random-(random*.075);
         
     }
    
 }
 
-employee.prototype.renderNameAndSalary=function(){
+Employee.prototype.renderNameAndSalary=function(){
     let num=this.employeeId_var-999;
   document.getElementById("name"+num).innerHTML=this.FullName_var;
   document.getElementById("salary"+num).innerHTML=this.salary_var;
